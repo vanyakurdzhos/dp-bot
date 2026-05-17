6002ffe76722826e6a2d5b020aebb10397373f30
Vanya <vanyakurdzhos4@gmail.com>
2026-05-17 18:43:42 +0200

    added rules command

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -10,14 +9,61 @@ const client = new Client({
 });
 
 client.once('ready', () => {
-    console.log('Bot je online!');
+    console.log('🚍 DP Šakvice bot je online!');
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async (message) => {
+
     if (message.author.bot) return;
 
-    if (message.content === '!ping') {
-        message.reply('Pong! 🏓');
+    if (message.content === '!pravidla') {
+
+        const embed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('📜 Pravidlá Dopravného podniku Šakvice, a.s.')
+            .setDescription('Oficiálne pravidlá Dopravného podniku Šakvice.')
+            .addFields(
+                {
+                    name: '§1 Základné pravidlá',
+                    value:
+                    '• Každý zamestnanec je povinný správať sa slušne a profesionálne.\n' +
+                    '• Rešpekt voči vedeniu, kolegom aj občanom je povinný.\n' +
+                    '• Vyvolávanie konfliktov a toxické správanie je zakázané.'
+                },
+                {
+                    name: '§2 Služba a aktivita',
+                    value:
+                    '• Služba musí byť vykonávaná realisticky.\n' +
+                    '• AFK počas služby bez dôvodu nie je povolené.\n' +
+                    '• Dlhodobá neaktivita môže viesť k vyradeniu.'
+                },
+                {
+                    name: '§3 Vozidlá',
+                    value:
+                    '• So služobnými vozidlami sa jazdí opatrne.\n' +
+                    '• Úmyselné ničenie vozidiel je zakázané.\n' +
+                    '• Vozidlá musia byť odstavené na určenom mieste.'
+                },
+                {
+                    name: '§4 Komunikácia',
+                    value:
+                    '• Komunikácia musí byť slušná.\n' +
+                    '• Spamovanie je zakázané.\n' +
+                    '• Rešpektovanie vedenia je povinné.'
+                },
+                {
+                    name: '§5 Tresty',
+                    value:
+                    '• upozornenie\n' +
+                    '• pokarhanie\n' +
+                    '• suspendácia\n' +
+                    '• vyradenie z podniku'
+                }
+            )
+            .setFooter({ text: '🚍 Dopravný podnik Šakvice, a.s.' })
+            .setTimestamp();
+
+        message.channel.send({ embeds: [embed] });
     }
 });
 
